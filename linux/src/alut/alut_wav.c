bb231c3d4f3b5cab89a8766168b22c7da42173d9
jvalenzu <jvalenzu@109fce75-3012-0410-ac6a-932efec96c1b>
2002-08-26 18:25:40 +0000

    Mon Aug 26 11:14:44 PDT 2002 	- recommitting
    git-svn-id: svn://connect.creativelabs.com/trunk@79 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/src/alut/alut_wav.c
+++ linux/src/alut/alut_wav.c
@@ -31,6 +31,9 @@
 static ALboolean ReadWAVFile(const char *fname, void **pcmdata,
 			ALushort *rfmt, ALushort *rchan,
 			ALushort *rfreq, ALuint *rsize);
+static ALboolean ReadWAVMemory(const ALvoid *data, void **pcmdata,
+			       ALushort *rfmt, ALushort *rchan,
+			       ALushort *rfreq, ALuint *rsize);
 
 ALboolean alutLoadWAV( const char *fname,
                         void **wave,
@@ -64,9 +67,12 @@ ALboolean alutLoadWAV( const char *fname,
 	return AL_TRUE;
 }
 
-static ALboolean ReadWAVFile(const char *fname, void **pcmdata,
-			ALushort *rfmt, ALushort *rchan, ALushort *rfreq,
-			ALuint *rsize) {
+static ALboolean ReadWAVFile(const char *fname,
+			     void **pcmdata,
+			     ALushort *rfmt,
+			     ALushort *rchan,
+			     ALushort *rfreq,
+			     ALuint *rsize) {
 	void *data;
 	ALint len;  /* length of pcm portion */
 	void *udata;
@@ -105,4 +111,120 @@ static ALboolean ReadWAVFile(const char *fname, void **pcmdata,
 		*rfreq, *rsize, *rfmt);
 
 	return AL_TRUE;
+}
+
+static ALboolean ReadWAVMemory(const ALvoid *data,
+			       void **pcmdata,
+			       ALushort *rfmt,
+			       ALushort *rchan,
+			       ALushort *rfreq,
+			       ALuint *rsize)
+{
+	void *mutable;
+	ALuint len;
+	void *udata;
+
+	if((rfmt == NULL) || (rchan == NULL) || (rfreq == NULL)) {
+		/* bad mojo */
+		return AL_FALSE;
+	}
+
+	/*
+	 * o/~ I'm an asshole o/~
+	 *
+	 * Don't worry, it should be safe
+	 */
+	memcpy(&mutable, &data, sizeof(void *));
+
+	if(acLoadWAV(mutable,
+		     &len,
+		     &udata,
+		     rfmt,
+		     rchan,
+		     rfreq) == NULL)
+	{
+		_alDebug(ALD_CONVERT, __FILE__, __LINE__,
+			"Could not buffer and convert data");
+
+		return AL_FALSE;
+	}
+
+	*rfmt    = _al_AC2ALFMT(*rfmt, *rchan);
+	*rsize   = len;
+	*pcmdata = udata;
+
+	_alDebug(ALD_CONVERT, __FILE__, __LINE__,
+		"ReadWAVMemory [freq/size/acformat] = [%d/%d/0x%x]",
+		*rfreq, *rsize, *rfmt);
+
+	return AL_TRUE;
+}
+
+/* I'm such a nice guy */
+ALvoid alutLoadWAVFile(ALbyte *fname,
+		       ALenum *format,
+		       ALvoid **data,
+		       ALsizei *size,
+		       ALsizei *freq,
+		       ALboolean *loop)
+{
+	ALboolean ret;
+	ALsizei bits_dummy;
+
+	ret = alutLoadWAV( (const char *) fname,
+			   data,
+			   format,
+			   size,
+			   &bits_dummy,
+			   freq );
+
+	if(loop)
+	{
+		*loop = AL_FALSE;
+	}
+
+	return;
+}
+
+void alutLoadWAVMemory(ALbyte *memory,
+		       ALenum *format,
+		       ALvoid **data,
+		       ALsizei *size,
+		       ALsizei *freq,
+		       ALboolean *loop)
+{
+	ALushort alFmt  = 0;
+	ALushort acChan = 0;
+	ALushort acFreq = 0;
+	ALuint   acSize = 0;
+
+	if(!ReadWAVMemory(memory, data,
+			&alFmt, &acChan, &acFreq, &acSize))
+	{
+		_alDebug(ALD_CONVERT, __FILE__, __LINE__,
+			"ReadWAVMemory failed");
+
+		return;
+	}
+
+	/* set output params */
+	*format = (ALsizei) alFmt;
+	*freq   = (ALsizei) acFreq;
+	*size   = (ALsizei) acSize;
+	*loop   = AL_FALSE; /* JIV FIXME */
+	/* *bits   = (ALsizei) _al_formatbits(alFmt); */
+	
+	_alDebug(ALD_CONVERT, __FILE__, __LINE__,
+		"alutLoadWAVMemory with [format/size/bits/freq] = [0x%x/%d/%d]",
+		*format, *size, *freq);
+
+	return;
+}
+
+void alutUnloadWAV(UNUSED(ALenum format),
+		   ALvoid *data,
+		   UNUSED(ALsizei size),
+		   UNUSED(ALsizei freq))
+{
+	free(data);
 }
