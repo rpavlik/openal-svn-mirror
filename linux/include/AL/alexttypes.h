a666cf79cf0f6f38f45125841ff07d6281656ead
jvalenzu <jvalenzu@109fce75-3012-0410-ac6a-932efec96c1b>
2003-01-13 22:02:58 +0000

    Added LAL_OPENAL
    git-svn-id: svn://connect.creativelabs.com/trunk@172 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/include/AL/alexttypes.h
+++ linux/include/AL/alexttypes.h
@@ -1,6 +1,8 @@
 #ifndef _LAL_EXTTYPES_H_
 #define _LAL_EXTTYPES_H_
 
+#define LAL_OPENAL                                1
+
 /* format base 0x10000 */
 #define AL_FORMAT_IMA_ADPCM_MONO16_EXT            0x10000
 #define AL_FORMAT_IMA_ADPCM_STEREO16_EXT          0x10001
