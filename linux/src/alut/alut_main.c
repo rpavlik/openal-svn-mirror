35c1d1fa8fa25abacc6094ce74358cf90cfad2aa
svenp <svenp@109fce75-3012-0410-ac6a-932efec96c1b>
2005-04-17 16:52:32 +0000

    Don't set an ALC error condition. Close the device in alutExit. Don't use a global variable for the context, use the current context instead, just like the other ALUT implementations.
    git-svn-id: svn://connect.creativelabs.com/trunk@451 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/src/alut/alut_main.c
+++ linux/src/alut/alut_main.c
@@ -6,45 +6,28 @@
  * Top level functions for alut stuff.
  */
 #include "al_siteconfig.h"
-#include "al_types.h"
 #include "al_main.h"
-#include "alc/alc_error.h"
 
-#include <AL/al.h>
 #include <AL/alc.h>
 #include <AL/alut.h>
 
-#include <stdio.h>
-
-/* FIXME: thread safe? */
-static void *alut_context_id = NULL;
-
 void alutInit(UNUSED(int *argc), UNUSED(char *argv[])) {
-	ALCdevice *dev;
-
-	dev = alcOpenDevice( NULL );
-	if( dev == NULL ) {
-		_alcSetError( ALC_INVALID_DEVICE );
+	ALCdevice *device = alcOpenDevice( NULL );
+	if( device != NULL ) {
+		ALCcontext *context = alcCreateContext( device, NULL );
+		if( context != NULL ) {
+			alcMakeContextCurrent( context );
+		}
 	}
-
-	alut_context_id = alcCreateContext( dev, NULL );
-
-	if(alut_context_id == NULL) {
-		_alcSetError( ALC_INVALID_DEVICE );
-	}
-
-	alcMakeContextCurrent( alut_context_id );
-
-	return;
 }
 
 void alutExit(void) {
-	if(alut_context_id == NULL) {
-		_alcSetError(ALC_INVALID_CONTEXT);
-		return;
+	ALCcontext *context = alcGetCurrentContext();
+	if( context != NULL ) {
+		ALCdevice *device = alcGetContextsDevice( context );
+		alcDestroyContext( context );
+		if( device != NULL ) {
+			alcCloseDevice( device );
+		}
 	}
-
-	alcDestroyContext(alut_context_id);
-
-	return;
 }
