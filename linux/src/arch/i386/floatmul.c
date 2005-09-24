2f5cf94fa28ab8b06d2e67781b3384e3aa087316
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2005-09-24 08:48:41 +0000

    fix up and drop unneeded saturation
    git-svn-id: svn://connect.creativelabs.com/trunk@652 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/src/arch/i386/floatmul.c
+++ linux/src/arch/i386/floatmul.c
@@ -5,28 +5,18 @@
 #define SCALING_POWER  9
 #define SCALING_FACTOR (1<<SCALING_POWER)
 
-void float_mul(ALshort *bpt, ALfloat sa, ALuint len, ALint min, ALint max);
+void _alFloatMul(ALshort *bpt, ALfloat sa, ALuint len);
 
-void float_mul(ALshort *bpt, ALfloat sa, ALuint len, ALint min, ALint max) {
-	ALint scaled_sa  = sa  * SCALING_FACTOR;
-	ALint scaled_max = max * SCALING_FACTOR;
-	ALint scaled_min = min * SCALING_FACTOR;
+void _alFloatMul(ALshort *bpt, ALfloat sa, ALuint len) {
+	ALint scaled_sa = sa * SCALING_FACTOR;
 	ALint iter;
 
 	while(len--) {
 		iter = *bpt;
 		iter *= scaled_sa;
-
-		if(iter > scaled_max) {
-			*bpt = max;
-		} else if (iter < scaled_min) {
-			*bpt = min;
-		} else {
-			iter /= SCALING_FACTOR;
-			*bpt = iter;
-		}
-
-		bpt++;
+		iter >>= SCALING_POWER;
+		*bpt = iter;
+		++bpt;
 	}
 
 	return;
