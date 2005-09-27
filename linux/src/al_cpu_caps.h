a954d683e2ff8515a5ed934cfae738b2bfba83f9
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2005-09-27 17:38:47 +0000

    add x86 cpu capabilities detection
    git-svn-id: svn://connect.creativelabs.com/trunk@670 109fce75-3012-0410-ac6a-932efec96c1b

---
+++ linux/src/al_cpu_caps.h
@@ -0,0 +1,10 @@
+#ifndef AL_CPU_CAPS_H
+#define AL_CPU_CAPS_H
+
+void _alDetectCPUCaps(void);
+
+#if defined(__i386__) || defined(__x86_64__)
+#include "x86_cpu_caps_prk.h"
+#endif
+
+#endif /* AL_CPU_CAPS_H */
---
+++ linux/src/arch/portable/cpu_caps.c
@@ -0,0 +1,6 @@
+#include "al_cpu_caps.h"
+
+/* stub to make linker happy */
+void _alDetectCPUCaps()
+{
+}
