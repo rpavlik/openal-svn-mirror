219e6631a213df4da9272cfb4681c946df6bbfad
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2005-11-21 10:46:00 +0000

    move CMakeModules
    git-svn-id: svn://connect.creativelabs.com/trunk@765 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/admin/CMakeModules/CMakeDetermineASMCompiler.cmake
+++ linux/admin/CMakeModules/CMakeDetermineASMCompiler.cmake
@@ -5,7 +5,7 @@ MARK_AS_ADVANCED(CMAKE_ASM_COMPILER)
 
 
 # configure variables set in this file for fast reload later on
-CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/CMakeModules/CMakeASMCompiler.cmake.in 
+CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/admin/CMakeModules/CMakeASMCompiler.cmake.in 
                ${CMAKE_BINARY_DIR}/CMakeFiles/CMakeASMCompiler.cmake IMMEDIATE)
 
 SET(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
