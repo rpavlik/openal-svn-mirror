542b3308fb0f229e1e95f223a7c183a16cee2009
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-01-11 20:21:58 +0000

    #if -> #ifdef
    git-svn-id: svn://connect.creativelabs.com/trunk@935 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/src/al_siteconfig.h
+++ linux/src/al_siteconfig.h
@@ -11,7 +11,7 @@
 #include "config.h"
 #endif /* DARWIN_PBBUILDER */
 
-#if HAVE___ATTRIBUTE__
+#ifdef HAVE___ATTRIBUTE__
 #define UNUSED(x) x __attribute__((unused))
 #define AL_ATTRIBUTE_FORMAT_PRINTF_(x,y) __attribute__((format(printf, x, y)))
 #define AL_ATTRIBUTE_NORETURN_ __attribute__((noreturn))
