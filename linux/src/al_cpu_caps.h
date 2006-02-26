0e3b7ea7e6a406dc9c5115f99ab2943cbc5b9a66
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-02-26 15:45:20 +0000

    respect msvc defines
    git-svn-id: svn://connect.creativelabs.com/trunk@1072 109fce75-3012-0410-ac6a-932efec96c1b

--- linux/src/al_cpu_caps.h
+++ linux/src/al_cpu_caps.h
@@ -3,7 +3,7 @@
 
 void _alDetectCPUCaps(void);
 
-#if defined(__i386__) || defined(__x86_64__)
+#if defined(__i386__) || defined(__x86_64__) || defined(_M_IX86) || defined(_M_X64)
 #include "arch/i386/x86_cpu_caps_prk.h"
 #endif
 
