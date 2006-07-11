33f9971f12293d77b34f6776743cc680a4133e34
svenp <svenp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-07-11 14:24:51 +0000

    MMSYSTEM backend fixes based on patches from Brian Keener <bkeener@thesoftwaresource.com>. Not really tested, but at least things compile under Cygwin.
    git-svn-id: svn://connect.creativelabs.com/trunk@1263 109fce75-3012-0410-ac6a-932efec96c1b

--- OpenAL-Sample/src/backends/alc_backend_windows.c
+++ OpenAL-Sample/src/backends/alc_backend_windows.c
@@ -1,36 +1,29 @@
-/* vi:set ts=8:
- *
- * windows_native.c
- *
- * functions related to the aquisition and management of the native
- * audio on Windows.
- *
+/*
+ * MMSYSTEM backend for Windows
  */
 
 #include "al_siteconfig.h"
-#include <stdlib.h>
 #include "backends/alc_backend.h"
+#include <stdlib.h>
 
 #ifndef USE_BACKEND_NATIVE_WINDOWS
 
-void alcBackendOpenNative_ (UNUSED(ALC_OpenMode mode), UNUSED(ALC_BackendOps **ops),
-			    ALC_BackendPrivateData **privateData)
+void
+alcBackendOpenNative_ (UNUSED (ALC_OpenMode mode),
+                       UNUSED (ALC_BackendOps **ops),
+                       struct ALC_BackendPrivateData **privateData)
 {
-	*privateData = NULL;
+  *privateData = NULL;
 }
 
 #else
 
 #include <AL/al.h>
-#include <stdio.h>
 #include <windows.h>
-#include <mmsystem.h>
 
 #include "al_main.h"
 #include "al_debug.h"
-
 #include "alc/alc_context.h"
-
 #include "al_mutexlib.h"
 
 #define DEF_BITSPERSAMPLE 16
@@ -41,296 +34,293 @@ void alcBackendOpenNative_ (UNUSED(ALC_OpenMode mode), UNUSED(ALC_BackendOps **o
 
 #define MAX_AUDIOBUFS     8
 #define MAX_PCMDATA       (DEF_SPEED * DEF_CHANNELS)
-#define EMAIL_ADDRESS     "tsaotsao@lokigames.com"
 
 static MutexID mutex;
 
-static struct {
-	WAVEHDR whdrs[MAX_AUDIOBUFS];
-	int index;
+static struct
+{
+  WAVEHDR whdrs[MAX_AUDIOBUFS];
+  int index;
+  int freecount;
 } audiobufs;
 
-static struct {
-        HWAVEOUT hwo;
-	WAVEFORMATEX pwfx;
-        ALC_OpenMode mode;
+static struct
+{
+  HWAVEOUT hwo;
+  WAVEFORMATEX pwfx;
 } WinAudioHandle;
 
 static char pcmdata[MAX_PCMDATA];
 static int scalesize = 0;
+static CRITICAL_SECTION waveCriticalSection;
 
+static void CALLBACK
+WinFillAudio (HWAVEOUT hwo, UINT uMsg, DWORD dwInstance, DWORD dwParam1,
+              DWORD dwParam2)
+{
+  int *freeBlockCounter = (int *) dwInstance;
 
-static void CALLBACK WinFillAudio(UNUSED(HWAVEOUT hwo),
-	UNUSED(UINT uMsg),
-	UNUSED(DWORD dwInstance),
-	UNUSED(DWORD dwParam1),
-	UNUSED(DWORD dwParam2)) {
+  /* Only service "buffer done playing" messages */
+  if (uMsg != WOM_DONE)
+    {
+      return;
+    }
+  /* _alUnlockMutex(mutex); not sure if we need this */
 
-        /* Only service "buffer done playing" messages */
-        if( uMsg == WOM_DONE ) {
-		_alUnlockMutex(mutex);
-	}
-
-	return;
-}
-
-
-static const char *implement_me(const char *fn) {
-	static char retval[2048];
-
-	snprintf(retval, sizeof(retval),
-	"%s is not implemented under Windows.  Please contact %s for\n"
-	"information on how you can help get %s implemented on Windows.\n",
-	fn, EMAIL_ADDRESS, fn);
-
-	return retval;
-}
-
-static void *grab_read_native(void) {
-	return NULL;
-}
-
-static void *grab_write_native(void) {
-	MMRESULT err;
-	LPWAVEFORMATEX pwfx = &WinAudioHandle.pwfx;
-	int i;
-
-	audiobufs.index = 0;
-	mutex = _alCreateMutex();
-
-	for(i = 0; i < MAX_AUDIOBUFS; i++) {
-		audiobufs.whdrs[i].lpData  = NULL;
-		audiobufs.whdrs[i].dwBufferLength = 0;
-		audiobufs.whdrs[i].dwFlags = WHDR_DONE;
-	}
-
-	memset(pwfx, 0, sizeof *pwfx);
-
-	pwfx->wFormatTag      = DEF_FORMAT;
-	pwfx->nChannels       = DEF_CHANNELS;
-	pwfx->wBitsPerSample  = DEF_BITSPERSAMPLE;
-	pwfx->nBlockAlign     = DEF_CHANNELS * (pwfx->wBitsPerSample / 8);
-	pwfx->nSamplesPerSec  = DEF_SPEED;
-	pwfx->nAvgBytesPerSec = pwfx->nSamplesPerSec * pwfx->nBlockAlign;
- 	pwfx->cbSize          = DEF_CBSIZE;
-
-	err = waveOutOpen(&WinAudioHandle.hwo, WAVE_MAPPER, pwfx,
-		(DWORD) WinFillAudio,
-		0,
-		CALLBACK_FUNCTION);
-
-	if(err == MMSYSERR_NOERROR) {
-		fprintf(stderr, "got Windows native audio\n");
-		WinAudioHandle.mode = ALC_OPEN_OUTPUT_;
-		return &WinAudioHandle.hwo;
-	}
-
-	fprintf(stderr, "Could not open Windows native audio: %d\n",
-		err);
-
-	return NULL;
-}
-
-static void native_blitbuffer(void *handle, const void *dataptr, int bytesToWrite) {
-	WAVEHDR *whdr;
-	MMRESULT err;
-        HWAVEOUT hwo;
-	char *bufptr;
-	char errmsg[256];
-
-	if(handle == NULL) {
-		return;
-	}
-
-	hwo = WinAudioHandle.hwo;
-
-	_alLockMutex(mutex);
-
-	whdr   = &audiobufs.whdrs[audiobufs.index];
-
-	do {
-		/* busy wait, ouch */
-		_alMicroSleep(0);
-	} while((whdr->dwFlags & WHDR_DONE) == 0);
-
-	bufptr = pcmdata + audiobufs.index * scalesize;
-	memcpy(bufptr, dataptr, bytesToWrite);
-
-	audiobufs.index = (audiobufs.index + 1) % MAX_AUDIOBUFS;
-
-	err = waveOutWrite(hwo, whdr, sizeof *whdr);
-	if(err != MMSYSERR_NOERROR) {
-		waveOutGetErrorText(err, errmsg, 256);
-		fprintf(stderr, "native_blitbuffer: write error %s\n",
-			errmsg);
-	}
-
-	return;
+  EnterCriticalSection (&waveCriticalSection);
+  (*freeBlockCounter)++;
+  LeaveCriticalSection (&waveCriticalSection);
 }
 
 /*
  * FIXME: unprepare whdrs
  */
-static void release_native(void *handle) {
-	WAVEHDR *whdr;
-	HWAVEOUT hwo;
-	int i;
+static void
+closeMMSYSTEM (struct ALC_BackendPrivateData *privateData)
+{
+  MMRESULT err;
+  WAVEHDR *whdr;
+  HWAVEOUT hwo;
+  int i;
+  char errmsg[256];
 
-	_alDestroyMutex(mutex);
-	mutex = NULL;
+  _alDestroyMutex (mutex);
+  mutex = NULL;
+  DeleteCriticalSection (&waveCriticalSection);
 
-	if(handle == NULL) {
-		return;
-	}
+  hwo = WinAudioHandle.hwo;
+  whdr = &audiobufs.whdrs[audiobufs.index];
+  while (audiobufs.freecount < MAX_AUDIOBUFS)
+    {
+      _alMicroSleep (10);
+    }
 
-	hwo = WinAudioHandle.hwo;
-	whdr = &audiobufs.whdrs[audiobufs.index];
+  for (i = 0; i < MAX_AUDIOBUFS; i++)
+    {
+      /* not first time */
+      if (audiobufs.whdrs[i].dwFlags & WHDR_PREPARED)
+        waveOutUnprepareHeader (hwo, &audiobufs.whdrs[i], sizeof (WAVEHDR));
+    }
 
-	for(i = 0; i < MAX_AUDIOBUFS; i++) {
-		/* not first time */
-		waveOutUnprepareHeader(hwo, whdr, sizeof *whdr);
-	}
+  err = waveOutReset (hwo);
+  if (err != MMSYSERR_NOERROR)
+    {
+      waveOutGetErrorText (err, errmsg, sizeof (errmsg));
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "waveOutReset failed: %s",
+                errmsg);
+    }
 
-	waveOutClose(hwo);
-
-	return;
+  err = waveOutClose (hwo);
+  if (err != MMSYSERR_NOERROR)
+    {
+      waveOutGetErrorText (err, errmsg, sizeof (errmsg));
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "waveOutClose failed: %s",
+                errmsg);
+    }
 }
 
-static int set_nativechannel(UNUSED(void *handle),
-		      UNUSED(ALuint channel),
-		      UNUSED(ALfloat volume)) {
-	fprintf(stderr,  implement_me("set_nativechannel"));
-
-	return 0;
+static void
+pauseMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData))
+{
+  return;
 }
 
-static void pause_nativedevice(UNUSED(void *handle)) {
-	fprintf(stderr,  implement_me("pause_nativedevice"));
-
-	return;
-}
-
-static void resume_nativedevice(UNUSED(void *handle)) {
-	fprintf(stderr,  implement_me("resume_nativedevice"));
-
-	return;
-}
-
-static ALfloat get_nativechannel(UNUSED(void *handle), UNUSED(ALuint channel)) {
-	fprintf(stderr,  implement_me("get_nativechannel"));
-
-	return 0.0;
-}
-
-static ALsizei capture_nativedevice(UNUSED(void *handle),
-			  UNUSED(void *capture_buffer),
-			  UNUSED(int bytesToRead)) {
-	return 0; /* unimplemented */
-}
-
-
-static ALboolean set_write_native(UNUSED(void *handle),
-				  UNUSED(unsigned int *deviceBufferSizeInBytes),
-				  UNUSED(ALenum *fmt),
-				  UNUSED(unsigned int *speed)) {
-	WAVEHDR *whdr;
-	MMRESULT err;
-	LPWAVEFORMATEX pwfx = &WinAudioHandle.pwfx;
-	int i;
-	char *bufptr;
-        HWAVEOUT hwo;
-	ALuint channels = _alGetChannelsFromFormat(*fmt);
-
-	/* close previous instance */
-	waveOutClose(WinAudioHandle.hwo);
-
-	memset(pwfx, 0, sizeof *pwfx);
-
-	pwfx->wFormatTag      = DEF_FORMAT;
-	pwfx->nChannels       = channels;
-	pwfx->wBitsPerSample  = _alGetBitsFromFormat(*fmt);
-	pwfx->nBlockAlign     = pwfx->nChannels * (pwfx->wBitsPerSample / 8);
-	pwfx->nSamplesPerSec  = *speed;
-	pwfx->nAvgBytesPerSec = pwfx->nSamplesPerSec * pwfx->nBlockAlign;
- 	pwfx->cbSize          = DEF_CBSIZE;
-
-	fprintf(stderr, "chans %d bps %d bal %d sps %d abs %d cbs %d\n",
-		pwfx->nChannels,
-		pwfx->wBitsPerSample,
-		pwfx->nBlockAlign,
-		(int) pwfx->nSamplesPerSec,
-		(int) pwfx->nAvgBytesPerSec,
-		(int) pwfx->cbSize);
-
-	err = waveOutOpen(&WinAudioHandle.hwo, WAVE_MAPPER, pwfx,
-		(DWORD) WinFillAudio,
-		0,
-		CALLBACK_FUNCTION);
-
-	if(err != MMSYSERR_NOERROR) {
-		fprintf(stderr, "Could not setfmt: %d\n", err);
-		return AL_FALSE;
-	}
-
-	fprintf(stderr, "setfmt okay\n");
-
-	hwo = WinAudioHandle.hwo;
-
-	scalesize = *deviceBufferSizeInBytes;
-
-	for(i = 0; i < MAX_AUDIOBUFS; i++) {
-		whdr   = &audiobufs.whdrs[i];
-		bufptr = pcmdata + i * scalesize;
-
-		whdr->lpData  = (LPSTR) bufptr;
-		whdr->dwBufferLength = *deviceBufferSizeInBytes;
-		whdr->dwFlags = WHDR_DONE;
-
-		err = waveOutPrepareHeader(hwo, whdr, sizeof *whdr);
-
-		if(err != MMSYSERR_NOERROR) {
-			fprintf(stderr, "Could not prepare header: %d\n", err);
-
-			return AL_FALSE;
-		}
-	}
-
-	return AL_TRUE;
-}
-
-static ALboolean set_read_native(UNUSED(void *handle),
-				 UNUSED(unsigned int *deviceBufferSizeInBytes),
-				 UNUSED(ALenum *fmt),
-				 UNUSED(unsigned int *speed)) {
-	return AL_FALSE;
+static void
+resumeMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData))
+{
+  return;
 }
 
 static ALboolean
-alcBackendSetAttributesNative_(void *handle, ALuint *deviceBufferSizeInBytes, ALenum *fmt, ALuint *speed)
+setAttributesMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData),
+                       UNUSED (unsigned int *bufsiz), UNUSED (ALenum *fmt),
+                       UNUSED (unsigned int *speed))
 {
-	return WinAudioHandle.mode == ALC_OPEN_INPUT_ ?
-		set_read_native(handle, deviceBufferSizeInBytes, fmt, speed) :
-		set_write_native(handle, deviceBufferSizeInBytes, fmt, speed);
+  MMRESULT err;
+  LPWAVEFORMATEX pwfx = &WinAudioHandle.pwfx;
+  HWAVEOUT hwo;
+  ALuint channels = _alGetChannelsFromFormat (*fmt);
+
+  memset (pwfx, 0, sizeof *pwfx);
+
+  pwfx->wFormatTag = DEF_FORMAT;
+  pwfx->nChannels = channels;
+  pwfx->wBitsPerSample = _alGetBitsFromFormat (*fmt);
+  pwfx->nBlockAlign = pwfx->nChannels * (pwfx->wBitsPerSample / 8);
+  pwfx->nSamplesPerSec = *speed;
+  pwfx->nAvgBytesPerSec = pwfx->nSamplesPerSec * pwfx->nBlockAlign;
+  pwfx->cbSize = DEF_CBSIZE;
+
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__,
+            "chans %d bps %d bal %d sps %d abs %d cbs %d\n", pwfx->nChannels,
+            pwfx->wBitsPerSample, pwfx->nBlockAlign,
+            (int) pwfx->nSamplesPerSec, (int) pwfx->nAvgBytesPerSec,
+            (int) pwfx->cbSize);
+
+  err = waveOutOpen (&WinAudioHandle.hwo, WAVE_MAPPER, pwfx,
+                     (DWORD_PTR) WinFillAudio,
+                     (DWORD_PTR) & audiobufs.freecount, CALLBACK_FUNCTION);
+  if (err != MMSYSERR_NOERROR)
+    {
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "waveOutOpen failed: %d",
+                err);
+      return AL_FALSE;
+    }
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "setAttributesMMSYSTEM OK");
+
+  hwo = WinAudioHandle.hwo;
+  scalesize = *bufsiz;
+  return AL_TRUE;
+}
+
+static void
+writeMMSYSTEM (struct ALC_BackendPrivateData *privateData, const void *data,
+               int bytesToWrite)
+{
+  WAVEHDR *whdr;
+  MMRESULT err;
+  HWAVEOUT hwo;
+  char *bufptr;
+  char errmsg[256];
+
+  hwo = WinAudioHandle.hwo;
+
+  _alLockMutex (mutex);
+
+  whdr = &audiobufs.whdrs[audiobufs.index];
+
+  if (whdr->dwFlags & WHDR_PREPARED)
+    waveOutUnprepareHeader (hwo, whdr, sizeof (WAVEHDR));
+
+  bufptr = pcmdata + audiobufs.index * scalesize;
+  memcpy (bufptr, data, bytesToWrite);
+  whdr->lpData = (LPSTR) bufptr;
+  whdr->dwBufferLength = bytesToWrite;
+  whdr->dwFlags = 0;
+
+  err = waveOutPrepareHeader (hwo, whdr, sizeof (WAVEHDR));
+  if (err != MMSYSERR_NOERROR)
+    {
+      waveOutGetErrorText (err, errmsg, sizeof (errmsg));
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__,
+                "waveOutPrepareHeader failed: %s", errmsg);
+    }
+
+  err = waveOutWrite (hwo, whdr, sizeof (WAVEHDR));
+  if (err != MMSYSERR_NOERROR)
+    {
+      waveOutGetErrorText (err, errmsg, sizeof (errmsg));
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "waveOutWrite failed: %s",
+                errmsg);
+    }
+  EnterCriticalSection (&waveCriticalSection);
+  audiobufs.freecount--;
+  LeaveCriticalSection (&waveCriticalSection);
+  while (!audiobufs.freecount)
+    _alMicroSleep (10);
+
+  audiobufs.index = (audiobufs.index + 1) % MAX_AUDIOBUFS;
+
+  _alUnlockMutex (mutex);
+}
+
+static ALsizei
+readMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData),
+              UNUSED (void *data), UNUSED (int bytesToRead))
+{
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "should never happen");
+  return 0;
+}
+static ALfloat
+getAudioChannelMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData),
+                         UNUSED (ALuint channel))
+{
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__,
+            "MMSYSTEM backend does not support getting the volume");
+  return -1.0f;
+}
+
+static int
+setAudioChannelMMSYSTEM (UNUSED (struct ALC_BackendPrivateData *privateData),
+                         UNUSED (ALuint channel), UNUSED (ALfloat volume))
+{
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__,
+            "MMSYSTEM backend does not support setting the volume");
+  return -1;
 }
 
 static ALC_BackendOps nativeOps = {
-	release_native,
-	pause_nativedevice,
-	resume_nativedevice,
-	alcBackendSetAttributesNative_,
-	native_blitbuffer,
-	capture_nativedevice,
-	get_nativechannel,
-	set_nativechannel
+  closeMMSYSTEM,
+  pauseMMSYSTEM,
+  resumeMMSYSTEM,
+  setAttributesMMSYSTEM,
+  writeMMSYSTEM,
+  readMMSYSTEM,
+  getAudioChannelMMSYSTEM,
+  setAudioChannelMMSYSTEM
 };
 
-void
-alcBackendOpenNative_ (ALC_OpenMode mode, ALC_BackendOps **ops, ALC_BackendPrivateData **privateData)
+static void *
+grab_write_native (void)
 {
-	*privateData = (mode == ALC_OPEN_INPUT_) ? grab_read_native() : grab_write_native();
-	if (*privateData != NULL) {
-		*ops = &nativeOps;
-	}
+  MMRESULT err;
+  LPWAVEFORMATEX pwfx = &WinAudioHandle.pwfx;
+  int i;
+
+  audiobufs.index = 0;
+  audiobufs.freecount = MAX_AUDIOBUFS;
+  mutex = _alCreateMutex ();
+
+  for (i = 0; i < MAX_AUDIOBUFS; i++)
+    {
+      audiobufs.whdrs[i].lpData = NULL;
+      audiobufs.whdrs[i].dwBufferLength = 0;
+      audiobufs.whdrs[i].dwFlags = 0;   /* WHDR_DONE */
+    }
+
+  memset (pwfx, 0, sizeof *pwfx);
+
+  pwfx->wFormatTag = DEF_FORMAT;
+  pwfx->nChannels = DEF_CHANNELS;
+  pwfx->wBitsPerSample = DEF_BITSPERSAMPLE;
+  pwfx->nBlockAlign = DEF_CHANNELS * (pwfx->wBitsPerSample / 8);
+  pwfx->nSamplesPerSec = DEF_SPEED;
+  pwfx->nAvgBytesPerSec = pwfx->nSamplesPerSec * pwfx->nBlockAlign;
+  pwfx->cbSize = DEF_CBSIZE;
+
+  InitializeCriticalSection (&waveCriticalSection);
+
+  err = waveOutOpen (&WinAudioHandle.hwo, WAVE_MAPPER, pwfx,
+                     (DWORD_PTR) WinFillAudio,
+                     (DWORD_PTR) & audiobufs.freecount, CALLBACK_FUNCTION);
+  if (err != MMSYSERR_NOERROR)
+    {
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "waveOutOpen failed: %d",
+                err);
+      return NULL;
+    }
+
+  _alDebug (ALD_MAXIMUS, __FILE__, __LINE__, "MMSYSTEM OK");
+  return &WinAudioHandle.hwo;
+}
+
+void
+alcBackendOpenNative_ (ALC_OpenMode mode, ALC_BackendOps **ops,
+                       struct ALC_BackendPrivateData **privateData)
+{
+  if (mode == ALC_OPEN_INPUT_)
+    {
+      _alDebug (ALD_MAXIMUS, __FILE__, __LINE__,
+                "MMSYSTEM backend does not support input");
+      *privateData = NULL;
+      return;
+    }
+
+  *privateData = grab_write_native ();
+  if (*privateData != NULL)
+    {
+      *ops = &nativeOps;
+    }
 }
 
 #endif /* USE_BACKEND_NATIVE_WINDOWS */
