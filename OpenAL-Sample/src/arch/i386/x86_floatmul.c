350adfd0cc5a2908500fe07f33b1f24c41d6a26f
prakashp <prakashp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-08-14 15:20:28 +0000

    add SSE2 routine
    git-svn-id: svn://connect.creativelabs.com/trunk@1339 109fce75-3012-0410-ac6a-932efec96c1b

--- OpenAL-Sample/src/arch/i386/x86_floatmul.c
+++ OpenAL-Sample/src/arch/i386/x86_floatmul.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   MMX routine                                                           *
+ *   MMX, SSE2 routine                                                           *
  *   Copyright (C) 2005 by Prakash Punnoor                                 *
  *   prakash@punnoor.de                                                    *
  *                                                                         *
@@ -24,7 +24,7 @@
 #include "al_cpu_caps.h"
 #include "x86_simd_support_prk.h"
 
-/* MMX routine needs 16 */
+/* MMX and SSE2 routines need 16 */
 #define SCALING_POWER  16
 #define SCALING_FACTOR (1 << SCALING_POWER)
 
@@ -34,6 +34,64 @@ void _alFloatMul(ALshort *bpt, ALfloat sa, ALuint len) {
 	ALint scaled_sa = sa * SCALING_FACTOR;
 	ALint iter;
 	
+#ifdef __SSE2__
+	if (_alHaveSSE2()) {
+		v8hi v_sa;
+		ALuint samples_main;
+		ALuint samples_pre;
+		ALuint samples_post;
+		
+		
+		samples_pre = SSE2_ALIGN - (aint)bpt % SSE2_ALIGN;
+		samples_pre /= sizeof(ALshort);
+		samples_main = len - samples_pre;
+		samples_post = samples_main % 16;
+		samples_main = samples_main / 16;
+		len = samples_post;
+		
+		while(samples_pre--) {
+			iter = *bpt;
+			iter *= scaled_sa;
+			iter >>= SCALING_POWER;
+			*bpt = iter;
+			++bpt;
+		}
+		if (scaled_sa < (1 << 15)) {
+			/* we do signed multiplication, so 1 << 15 is the max */
+			/* FIXME: NON-GCC probably doesn't know __extension__ */
+			v_sa = __extension__(v8hi){scaled_sa, scaled_sa, scaled_sa, scaled_sa,
+									scaled_sa, scaled_sa, scaled_sa, scaled_sa};
+			
+			while (samples_main--) {
+				*(v8hi*)bpt = __builtin_ia32_pmulhw128(*(v8hi*)bpt, v_sa);
+				bpt += 8;
+				*(v8hi*)bpt = __builtin_ia32_pmulhw128(*(v8hi*)bpt, v_sa);
+				bpt += 8;
+			}
+		} else {
+			/* we lose 1 bit here, but well... */
+			v8hi temp;
+			short sa2 = scaled_sa >> 1;
+			v_sa = __extension__(v8hi){sa2, sa2, sa2, sa2,
+				sa2, sa2, sa2, sa2};
+			
+			while (samples_main--) {
+				/* work-around gcc 3.3.x bug */
+				const long num_shift = 1L;
+				
+				temp = __builtin_ia32_pmulhw128(*(v8hi*)bpt, v_sa);
+				*(v8hi*)bpt = __builtin_ia32_psllwi128(temp, num_shift);
+				bpt += 8;
+				temp = __builtin_ia32_pmulhw128(*(v8hi*)bpt, v_sa);
+				*(v8hi*)bpt = __builtin_ia32_psllwi128(temp, num_shift);
+				bpt += 8;
+			}
+		}
+	}
+#ifdef __MMX__
+	else
+#endif /* __MMX__ */
+#endif /* __SSE2__ */
 #ifdef __MMX__
 	if (_alHaveMMX()) {
 		union {
