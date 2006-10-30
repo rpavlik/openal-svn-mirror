b9b3876aed6b7d8f5231029f959897fe8fbbbed3
svenp <svenp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-10-30 09:58:58 +0000

    Reverted changes to dlopen stuff
    git-svn-id: svn://connect.creativelabs.com/trunk@1405 109fce75-3012-0410-ac6a-932efec96c1b

--- OpenAL-Sample/src/al_dlopen.c
+++ OpenAL-Sample/src/al_dlopen.c
@@ -3,21 +3,92 @@
 
 #ifdef USE_DLOPEN
 
+#include <dlfcn.h>
+
+int
+alDLInit_ (void)
+{
+  return 0;
+}
+
+int
+alDLExit_ (void)
+{
+  return 0;
+}
+
+AL_DLHandle
+alDLOpen_ (const char *file)
+{
+  return dlopen (file, RTLD_LAZY | RTLD_GLOBAL);
+}
+
+int
+alDLClose_ (AL_DLHandle handle)
+{
+  return dlclose (handle);
+}
+
 AL_DLDataPtr
 alDLDataSym_ (AL_DLHandle handle, const char *name)
 {
-	return (AL_DLDataPtr) dlsym (handle, name);
+  return (AL_DLDataPtr) dlsym (handle, name);
 }
 
 AL_DLFunPtr
 alDLFunSym_ (AL_DLHandle handle, const char *name)
 {
-	return (AL_DLFunPtr) dlsym (handle, name);
+  return (AL_DLFunPtr) dlsym (handle, name);
 }
 
-#else /* USE_DLOPEN */
+const char *
+alDLError_ (void)
+{
+  return dlerror ();
+}
 
-const char alDLErrorMessage[] =
-	"Dynamic linking on this platform is not supported.";
+#else
+
+int
+alDLInit_ (void)
+{
+  return 1;
+}
+
+int
+alDLExit_ (void)
+{
+  return 1;
+}
+
+AL_DLHandle
+alDLOpen_ (UNUSED (const char *file))
+{
+  return (AL_DLHandle) 0;
+}
+
+int
+alDLClose_ (UNUSED (AL_DLHandle handle))
+{
+  return 1;
+}
+
+AL_DLDataPtr
+alDLDataSym_ (UNUSED (AL_DLHandle handle), UNUSED (const char *name))
+{
+  return (AL_DLDataPtr) 0;
+}
+
+AL_DLFunPtr
+alDLFunSym_ (UNUSED (AL_DLHandle handle), UNUSED (const char *name))
+{
+  return (AL_DLFunPtr) 0;
+}
+
+const char *
+alDLError_ (void)
+{
+  return "Dynamic linking on this platform is not supported.";
+}
 
 #endif /* USE_DLOPEN */
--- OpenAL-Sample/src/al_dlopen.h
+++ OpenAL-Sample/src/al_dlopen.h
@@ -3,80 +3,21 @@
 
 typedef void *AL_DLDataPtr;
 typedef void (*AL_DLFunPtr) (void);
+
 typedef void *AL_DLHandle;
 
-#ifdef USE_DLOPEN
+extern int alDLInit_ (void);
 
-#include <dlfcn.h>
+extern int alDLExit_ (void);
 
-static __inline int alDLInit_ (void) {return 0;}
+extern AL_DLHandle alDLOpen_ (const char *file);
 
-static __inline int alDLExit_ (void) {return 0;}
+extern int alDLClose_ (AL_DLHandle handle);
 
-static __inline AL_DLHandle
-alDLOpen_ (const char *file)
-{
-	return dlopen (file, RTLD_LAZY | RTLD_GLOBAL);
-}
+extern AL_DLDataPtr alDLDataSym_ (AL_DLHandle handle, const char *name);
 
-static __inline int
-alDLClose_ (AL_DLHandle handle)
-{
-	return dlclose (handle);
-}
+extern AL_DLFunPtr alDLFunSym_ (AL_DLHandle handle, const char *name);
 
-/* not inlined to have warning only at one place */
-extern AL_DLDataPtr
-alDLDataSym_ (AL_DLHandle handle, const char *name);
-
-/* not inlined to have warning only at one place */
-extern AL_DLFunPtr
-alDLFunSym_ (AL_DLHandle handle, const char *name);
-
-static __inline const char *
-alDLError_ (void)
-{
-	return dlerror ();
-}
-
-#else /* USE_DLOPEN */
-
-extern const char alDLErrorMessage[];
-
-
-static __inline int alDLInit_ (void) {return 1;}
-
-static __inline int alDLExit_ (void) {return 1;}
-
-static __inline AL_DLHandle
-alDLOpen_ (UNUSED (const char *file))
-{
-	return (AL_DLHandle) 0;
-}
-
-static __inline int
-alDLClose_ (UNUSED (AL_DLHandle handle))
-{
-	return 1;
-}
-
-static __inline AL_DLDataPtr
-alDLDataSym_ (UNUSED (AL_DLHandle handle), UNUSED (const char *name))
-{
-	return (AL_DLDataPtr) 0;
-}
-
-static __inline AL_DLFunPtr
-alDLFunSym_ (UNUSED (AL_DLHandle handle), UNUSED (const char *name))
-{
-	return (AL_DLFunPtr) 0;
-}
-
-static __inline const char *
-alDLError_ (void)
-{
-	return alDLErrorMessage;
-}
-#endif /* USE_DLOPEN */
+extern const char *alDLError_ (void);
 
 #endif /* not AL_AL_DLOPEN_H_ */
